3b317be7be0c9f488e02795cddc5b7136d132d37
keshew <artyom.korotckov2017@yandex.ru>
2025-07-09 17:07:51 +0500

    Initial Commit

---
+++ MoodCalendar/MoodCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodCalendarApp.swift
+//  MoodCalendar
+//
+//  Created by Артём Коротков on 09.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoodCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
